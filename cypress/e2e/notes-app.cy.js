961437e947e5d7dd13553980ae91fd175a0fc53e
Durlav <durlavk98@gmail.com>
2023-03-07 10:35:17 +0530

    cypress backend testing

---
+++ cypress/e2e/notes-app.cy.js
@@ -0,0 +1,28 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'John Doe',
+      username: 'johndoe',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('log in').click()
+  })
+
+  it('user can login', function () {
+    cy.contains('log in').click()
+    cy.get('input:first').type('johndoe')
+    cy.get('input:last').type('password')
+  })
+
+  
+})
